d81403335b1eff509056f7e9faeaf8e4a85607af
Khiveley <kenneth.hiveley@gmail.com>
2021-07-13 21:25:00 -0500

    complete boolean expressions

---
+++ Labs/BooleanExpressions.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BooleanExpressions {
+
+  public static void main(String[] args) {
+    Scanner kb = new Scanner(System.in);
+    boolean bool1, bool2, bool3, bool4, bool5, bool6;
+    double num1, num2;
+
+    System.out.println("Please choose two numbers.");
+    System.out.print("First number: ");
+    num1 = kb.nextDouble();
+    System.out.print("Second number: ");
+    
+  }
+}
