089480ab10c84301a9fc1d316fe213690e088141
Khiveley <kenneth.hiveley@gmail.com>
2021-07-15 10:58:31 -0500

    complete objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,8 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int i=20, sum = 0;
+    for(i=1; i<= 20; ++i)
+    sum = sum + i;
+    System.out.println(sum);
+  }
+}
