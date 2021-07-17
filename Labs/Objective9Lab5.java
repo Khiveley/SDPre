f8922c25151d605b7aa3536cc7706a6d6cff596a
Khiveley <kenneth.hiveley@gmail.com>
2021-07-17 01:15:50 -0500

    complete objective9lab5

---
+++ Labs/Objective9Lab5.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class Objective9Lab5 {
+
+  public static void main(String[] args) {
+    Scanner kb = new Scanner (System.in);
+		double num1, num2;
+		boolean keepGoing = true;
+		int choice;
+		double answer = 0.0;
+
+		while (keepGoing) {
+      System.out.println("Please provide a number. ");
+      num1 = kb.nextDouble();
+
+      System.out.println("Please provide another number. ");
+      num2 = kb.nextDouble();
+			printMenu();
+			System.out.println("Which would you like to do? ");
+			choice = kb.nextInt();
+
+			switch (choice) {
+
+    case 1:
+    System.out.println("Sum of two numbers" + findSum(num1,num2));
+    break;
+    case 2:
+    System.out.println("Average of two numbers" + findAverage(num1,num2));
+    break;
+    case 3:
+    System.out.println("Average of two numbers" + calcTax(num1,num2));
+    break;
+    case 4:
+    keepGoing = false;
+    break;
+    default:
+    System.out.println("Invalid Entry.");
+		}
+  }
+		kb.close();
+	}
+	public static void printMenu() {
+		System.out.println();
+		System.out.println("========= MENU =========");
+		System.out.println("|                      |");
+		System.out.println("|   1. Add Numbers     |");
+		System.out.println("|   2. Find Average    |");
+		System.out.println("|   3. Calculate Tax   |");
+		System.out.println("|   4. Exit            |");
+		System.out.println("|                      |");
+		System.out.println("========================");
+		System.out.println();
+	}
+
+	public static double findSum(double x, double y) {
+		double sum = x + y;
+		return x + y;
+	}
+
+  public static double findAverage(double x, double y) {
+    return (x + y)/2;
+
+}
+  public static double calcTax(double x, double y) {
+    return (x+y * .083);
+  }
+
+}
