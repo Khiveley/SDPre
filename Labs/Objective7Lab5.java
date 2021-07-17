9540e4cbe2f0fee6c3939984d56ae9a3ee566c14
Khiveley <kenneth.hiveley@gmail.com>
2021-07-16 22:55:34 -0500

    complete objective7lab5

--- Labs/Objective7Lab5.java
+++ Labs/Objective7Lab5.java
@@ -3,26 +3,25 @@ import java.util.Scanner;
 public class Objective7Lab5 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    int selection;
-    int count = 1;
+    int selection = scanner.nextInt();
 
-    while (count == 1) {
+    while(true) {
       System.out.println("_____Menu_____");
       System.out.println("1: Say Hello");
       System.out.println("2: List My favorite foods");
       System.out.println("3: Exit");
       System.out.println();
-
-      selection = scanner.nextInt();
+{
       if(selection == 1) {
-        System.out.println=("Hello Human");
+        System.out.println("Hello Human");
       }
-      elseif (selection == 2) {
-        System.out.println=("Apples, Bananas, and Coconuts");
+      else if(selection == 2) {
+        System.out.println("Apples, Bananas, and Coconuts");
     }
-      elseif (selection == 3) {
-        System.out.println=("Goodbye");
-      }
+      else
+        System.out.println("Goodbye");
+        scanner.close();
     }
   }
+}
 }
